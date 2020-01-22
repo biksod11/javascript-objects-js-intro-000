d6834b8b1a91a3f307e0b4ec29049c7b78d5947f
Bikram Sodhi <biksodhi@gmail.com>
2020-01-22 23:21:55 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,5 +3,8 @@ var playlist = { "Just Bieber" : "Song Title"}
 function updatePlaylist(playlist, artist, title){
   playlist[artist] = title
   return playlist
-
+}
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist
+  return playlist
 }
