e2708e780b2e1d6c273e9aa4832da282367c249a
Bikram Sodhi <biksodhi@gmail.com>
2020-01-22 23:19:37 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = { "Just Bieber" : "Song Title"}
 
 function updatePlaylist(playlist, artist, title){
-  return object.assign({}, playlist, {[artist]:title})
+  playlist[artist] = Song
+  return playlsit
 
 }
